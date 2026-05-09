7b73b4fb988347d542f37dfc874b03928babd888
Michael Cho <michael@michaelcho.dev>
2026-05-09 13:45:05 -0400

    arcade-learning-environment: use `sdl2-compat`

--- Formula/a/arcade-learning-environment.rb
+++ Formula/a/arcade-learning-environment.rb
@@ -24,7 +24,7 @@ class ArcadeLearningEnvironment < Formula
   depends_on "numpy"
   depends_on "opencv"
   depends_on "python@3.14"
-  depends_on "sdl2"
+  depends_on "sdl2-compat"
 
   on_linux do
     depends_on "zlib-ng-compat"
